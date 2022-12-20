42bddbd53210d95165560067fd22d2f3f64ef029
lucasbeacon <lucas.nepomuceno2121@gmail.com>
2022-12-20 18:56:39 -0300

    Adicionei a variavel nome

--- index.php
+++ index.php
@@ -3,5 +3,10 @@
 echo "Hello GitHub!";
 echo "We need to learn HTML5 again... I don't think so...";
 
+<br>
+
+echo "from now on, I will be testing variables";
+$nome = Lucao;
+var_dump($nome)
 
 ?>
